8d316291ede86977d0decd9eafa7210cd9d4f90d
weicuinz <36883556+weicuinz@users.noreply.github.com>
2022-11-12 18:52:05 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast Menu')
@@ -7,3 +12,5 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
