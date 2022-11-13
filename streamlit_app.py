126d22f6cbbe13a001a284c80ad019196843b7f0
weicuinz <36883556+weicuinz@users.noreply.github.com>
2022-11-13 21:04:48 +1300

    Update streamlit_app.py
    Use fatchall()

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
